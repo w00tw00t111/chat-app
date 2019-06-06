23418f9be24a0a095d8b1737033d2df924548bff
w00tw00t111 <w00tw00t111@users.noreply.github.com>
2019-06-06 14:27:19 -0500

    Add Mustache Templating

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,17 +1,71 @@
 const socket = io()
 
-socket.on('welcomeMessage', (message)=>{
-    console.log(message)
-})
+//elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $geolocationButton = document.querySelector('#send-location')
+const $messages = document.querySelector('#messages')
 
-document.querySelector('#sendMessage').addEventListener('click',(e)=>{
-    e.preventDefault()
-    let message = document.querySelector('#messageText').value
-    //send message to node server
-    socket.emit('sendMessage', message)
-})
+//Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML
+const geolocationTemplate = document.querySelector('#geolocation-template').innerHTML
 
 //receive message from node server
 socket.on('message', (message)=>{
     console.log(message)
+    const html = Mustache.render(messageTemplate, {
+        message
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+})
+
+//receive user location from node server
+socket.on('locationMessage', (url)=>{
+    console.log(url)
+    const html = Mustache.render(geolocationTemplate,{
+        url
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+})
+
+$messageFormButton.addEventListener('click',(e)=>{
+    e.preventDefault()
+    //disable button
+    $messageFormButton.setAttribute('disabled','disabled')
+
+    let message = document.querySelector('#messageText').value
+    //send message to node server
+    socket.emit('sendMessage', message, (error)=>{
+        //after message is sent, re-enable and clear text box
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+        
+        if(error){
+            return console.log(error)
+        }
+        console.log('The message was delivered')
+    })
+})
+
+//Send current user's latitude and longitude location
+$geolocationButton.addEventListener('click', (e)=>{
+    if(!navigator.geolocation){
+        return alert('Geolocation is not supported by your browser.')
+    }
+    //disable button while location is being retrieved
+    $geolocationButton.setAttribute('disabled', 'disabled')
+
+    navigator.geolocation.getCurrentPosition((position)=>{
+        let latitude = position.coords.latitude
+        let longitude = position.coords.longitude
+
+        socket.emit('sendLocation', {latitude, longitude}, ()=>{
+            console.log('Location Shared')
+            //re-enable button after location is sent
+            $geolocationButton.removeAttribute('disabled')
+        })
+    })
+
 })
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
 const server = http.createServer(app)
@@ -15,11 +16,22 @@ app.use(express.static(publicDirectoryPath))
 io.on('connection', (socket)=>{
     console.log('New websocket connection')
 
-    socket.emit('welcomeMessage', 'Welcome!')
+    socket.emit('message', 'Welcome!')
     socket.broadcast.emit('message', 'A new user has joined')
 
-    socket.on('sendMessage', (message)=>{
+    socket.on('sendMessage', (message, callback)=>{
+        const filter = new Filter()
+
+        if(filter.isProfane(message)){
+            return callback('Profanity is not allowed')
+        }
         io.emit('message', message)
+        callback()
+    })
+
+    socket.on('sendLocation', (coordinates, callback)=>{
+        io.emit('locationMessage',`https://google.com/maps?q=${coordinates.latitude},${coordinates.longitude}`)
+        callback()
     })
 
     socket.on('disconnect', ()=>{
